bf37a97bc7b6f9fa3fac0deb1478f646dc0357c3
alaahahi <alaa.hahe@gmail.com>
2026-08-17 01:39:11 +0300

    s

---
+++ public/build/assets/ThemeSync-DBf-j_c7.js
@@ -0,0 +1,1 @@
+import{u as n}from"./useTheme-Dt3GMaxV.js";import{B as o,c as s,o as t}from"./app-CEn0viUs.js";const c={class:"d-none","aria-hidden":"true"},u={__name:"ThemeSync",setup(a){const{sync:e}=n();return o(e),(r,_)=>(t(),s("span",c))}};export{u as _};
