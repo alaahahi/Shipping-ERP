1c369db2e4c454ec619ebd17dde9e35a26395f50
alaahahi <alaa.hahe@gmail.com>
2026-08-17 01:49:18 +0300

    ششش

---
+++ public/build/assets/ThemeSync-NiAKcITD.js
@@ -0,0 +1,1 @@
+import{u as n}from"./useTheme-BYBowjr0.js";import{B as o,c as s,o as t}from"./app-CcuG-zCQ.js";const c={class:"d-none","aria-hidden":"true"},u={__name:"ThemeSync",setup(a){const{sync:e}=n();return o(e),(r,_)=>(t(),s("span",c))}};export{u as _};
---
+++ public/build/assets/useTheme-BYBowjr0.js
@@ -0,0 +1,1 @@
+import{B as o,Z as s,_ as r,$ as n,E as u}from"./app-CcuG-zCQ.js";const e=u(s());function h(){const a=()=>{e.value=s(),r(e.value)},t=()=>{e.value=n(e.value==="dark"?"light":"dark")};return o(a),{theme:e,toggleTheme:t,sync:a}}export{h as u};
