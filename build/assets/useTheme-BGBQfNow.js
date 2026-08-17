c5af6f834e5a3d14da41d304d874959b00e0c626
alaahahi <alaa.hahe@gmail.com>
2026-08-17 03:24:14 +0300

    d

--- build/assets/useTheme-BGBQfNow.js
+++
@@ -1,1 +0,0 @@
-import{G as o,_ as s,$ as r,a0 as n,J as u}from"./app-x_oh3l1z.js";const e=u(s());function h(){const a=()=>{e.value=s(),r(e.value)},t=()=>{e.value=n(e.value==="dark"?"light":"dark")};return o(a),{theme:e,toggleTheme:t,sync:a}}export{h as u};
