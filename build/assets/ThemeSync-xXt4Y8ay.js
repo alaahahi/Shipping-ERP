9d03e8597728877695e6a60a51f8276b25ced041
alaahahi <alaa.hahe@gmail.com>
2026-08-18 19:12:15 +0300

    a

---
+++ build/assets/ThemeSync-xXt4Y8ay.js
@@ -0,0 +1,1 @@
+import{u as n}from"./useTheme-DqD_1YBD.js";import{G as o,c as s,o as t}from"./app-BASGL2qJ.js";const c={class:"d-none","aria-hidden":"true"},u={__name:"ThemeSync",setup(a){const{sync:e}=n();return o(e),(r,_)=>(t(),s("span",c))}};export{u as _};
