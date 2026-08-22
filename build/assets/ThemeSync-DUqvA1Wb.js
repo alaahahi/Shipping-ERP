ba1f495ac89942efb4645b4541eddef9eb6fed91
alaahahi <alaa.hahe@gmail.com>
2026-08-22 15:25:47 +0300

    بب

---
+++ build/assets/ThemeSync-DUqvA1Wb.js
@@ -0,0 +1,1 @@
+import{u as n}from"./useTheme-BvQM28PZ.js";import{G as o,c as s,o as t}from"./app-CQbVHUED.js";const c={class:"d-none","aria-hidden":"true"},u={__name:"ThemeSync",setup(a){const{sync:e}=n();return o(e),(r,_)=>(t(),s("span",c))}};export{u as _};
