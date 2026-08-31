fa7528fd0b41b7a571645dd19dcc01e7b43ccdb6
alaahahi <alaa.hahe@gmail.com>
2026-08-31 18:10:29 +0300

    add

---
+++ build/assets/ThemeSync-Cf2p6Ub3.js
@@ -0,0 +1,1 @@
+import{u as n}from"./useTheme-BzK71UG5.js";import{G as o,c as s,o as t}from"./app-C23flfsR.js";const c={class:"d-none","aria-hidden":"true"},u={__name:"ThemeSync",setup(a){const{sync:e}=n();return o(e),(r,_)=>(t(),s("span",c))}};export{u as _};
